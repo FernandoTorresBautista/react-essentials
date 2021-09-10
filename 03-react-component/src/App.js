ac86351b97d068885623860f1b834a423d1245d8
Fernando Torres <fernandot371@gmail.com>
2021-09-10 18:35:11 -0500

    03-use of state

--- 03-react-component/src/App.js
+++ 03-react-component/src/App.js
@@ -1,34 +1,36 @@
+import {useState} from 'react';
 import './App.css';
 import Card from './Card';
 import faker from 'faker';
 
 function App() {
+  const [name, setName] = useState('Fer TB');
+  const [showCard, setShowCard] = useState(true);
   const buttonsMarkup = (
     <div>
       <button className="button button2">YES</button>
       <button className="button button3">NO</button>
     </div>
   );
+  const changeNameHandler = name => { setName(name) }
+  const changeInputHandler = e=>{ setName(e.target.value) }
+  const toggleShowCard = ()=>{ setShowCard(!showCard) }
+  const cardsMarkup =  showCard && <Card 
+    avatar={faker.image.avatar()}
+    name={name}
+    title='Lead Quality Producer' 
+    onChangeName={()=>changeNameHandler('Fer Tor')}
+    onChangeInput={changeInputHandler}
+    > {buttonsMarkup}
+  </Card>;
   return (
     <div className="App">
-      <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()} `}
-        title={ faker.name.jobTitle()} >
-        {buttonsMarkup}
-      </Card>
-      <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()} `}
-        title={ faker.name.jobTitle()} >
-        {buttonsMarkup}
-      </Card>
-      <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()} `}
-        title={ faker.name.jobTitle()} >
-        {buttonsMarkup}
-      </Card>
+      <button 
+        className="button" 
+        onClick={toggleShowCard}>
+        Toggle show/hide
+      </button>
+      { cardsMarkup }
     </div>
   );
 }
--- 03-react-component/src/Card.js
+++ 03-react-component/src/Card.js
@@ -5,11 +5,14 @@ const Card = (props) => {
   console.log(props);
   return (
     <div className="card">
-      <img src={props.avatar} alt="Avatar" style={{width:'100%'}} />
+      { /*<img src={props.avatar} alt="Avatar" style={{width:'100%'}} />*/ }
+      <img src="https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg" alt="Avatar" style={{width:'100%'}} />
       <div className="container">
         <h4><b>{props.name}</b></h4> 
         <p>{props.title}</p> 
-        <p>{props.children}</p> 
+        <input type="text" onChange={props.onChangeInput} value={props.name} />
+        <p><button className="button" onClick={props.onChangeName}>Change Name</button></p>
+        <div> {props.children} </div>
       </div>
     </div>
   )
