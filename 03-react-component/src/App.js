ea438d6867b694669f9bc324fd6738048435e5ba
Fernando Torres <fernandot371@gmail.com>
2021-09-15 18:34:37 -0500

    03-LifeCycle & useEffect convert funcional base to class base

--- 03-react-component/src/App.js
+++ 03-react-component/src/App.js
@@ -1,7 +1,7 @@
-import {useState} from 'react';
+import {useState, Component} from 'react';
 import './App.css';
 import Card from './Card';
-// import faker from 'faker';
+import faker from 'faker';
 import { ThemeProvider } from 'styled-components';
 import Button from './element/Button';
 
@@ -10,73 +10,81 @@ const theme = {
   mango: 'yellow'
 }
 
-function App() {
-  const [cards, setCards] = useState([
-    {
-      id:'asd',
-      name: "name 1",
-      title: "title 1",
-      avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
-    },
-    {
-      id:'sdf',
-      name: "name 2",
-      title: "title 2",
-      avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
-    },
-    {
-      id:'dfg',
-      name: "name 3",
-      title: "title 3",
-      avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
-    },
-  ]);
-  const [showCard, setShowCard] = useState(true);
-  const toggleShowCard = ()=>{ setShowCard(!showCard) }
+class  App extends Component  {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: [
+        {
+          id:'asd',
+          name: "name 1",
+          title: "title 1",
+          avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
+        },
+        {
+          id:'sdf',
+          name: "name 2",
+          title: "title 2",
+          avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
+        },
+        {
+          id:'dfg',
+          name: "name 3",
+          title: "title 3",
+          avatar: 'https://cdn.fakercloud.com/avatars/ceekaytweet_128.jpg'
+        },
+      ],
+      showCard: true
+    }
+  }
 
-  const deleteCardHandler = (cardIndex) => {
-    const cardsCopy = [...cards];
+  toggleShowCard = ()=> this.setState({ showCard: !this.state.showCard })
+
+  deleteCardHandler = (cardIndex) => {
+    const cardsCopy = [...this.state.cards];
     cardsCopy.splice(cardIndex,1);
-    setCards(cardsCopy);
+    this.setState({cards:cardsCopy})
   }
 
-  const changeNameHandler = (event, id) => {
-    const cardIndex = cards.findIndex(card=>card.id === id); // which card
-    const cardsCopy = [...cards]; // make a copy of the cards
+  changeNameHandler = (event, id) => {
+    const cardIndex = this.state.cards.findIndex(card=>card.id === id); // which card
+    const cardsCopy = [...this.state.cards]; // make a copy of the cards
     cardsCopy[cardIndex].name = event.target.value; // change the name of the specific card
-    setCards(cardsCopy); // set the cards with the latest change
+    this.setState({cards:cardsCopy})
   }
 
-  const classes = ['button'];
-  if (cards.length < 3) classes.push('pink');
-  if (cards.length < 2) classes.push('red');
-  const cardsMarkup =  showCard && (
-    cards.map((card, index) => 
-      <Card 
-        avatar={card.avatar}
-        name={card.name}
-        title={card.title}
-        key={card.id}
-        onDelete={()=>deleteCardHandler(index)}
-        onChangeName={(event)=>changeNameHandler(event,card.id)}
-      />)
-    );
-  
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Button color="mango" length={cards.length}>
-          Toggle
-        </Button>
-        <button 
-          className={classes.join(' ')} 
-          onClick={toggleShowCard}>
-          Toggle show/hide
-        </button>
-        { cardsMarkup }
-      </div>
-    </ThemeProvider>
-  );
+  render() {
+    const classes = ['button'];
+    if (this.state.cards.length < 3) classes.push('pink');
+    if (this.state.cards.length < 2) classes.push('red');
+    const cardsMarkup =  this.state.showCard && (
+      this.state.cards.map((card, index) => 
+        <Card 
+          avatar={card.avatar}
+          name={card.name}
+          title={card.title}
+          key={card.id}
+          onDelete={()=>this.deleteCardHandler(index)}
+          onChangeName={(event)=>this.changeNameHandler(event,card.id)}
+        />)
+      );
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Button color="mango" length={this.state.cards.length} onClick={this.toggleShowCard}>
+            Toggle
+          </Button>
+          <button 
+            className={classes.join(' ')} 
+            onClick={this.toggleShowCard}>
+            Toggle show/hide
+          </button>
+          { cardsMarkup }
+        </div>
+      </ThemeProvider>
+    )
+  }
+
 }
 
 export default App;
