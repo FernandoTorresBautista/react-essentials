c5bf9ba766c2ebce25df5866171282b3002ff488
Fernando TB <fernandot371@gmail.com>
2021-09-24 18:01:00 -0500

    06-use useReducer with useEffect to fetch data

---
+++ 06-usereducer-fetch/src/App.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useReducer} from 'react';
+import axios from 'axios';
+import './App.css';
+
+const initialState = {
+  loading: true, 
+  error: "",
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_DATA':
+      return {
+        loading: false, 
+        error: "",
+        todos: action.payload,      
+      }
+    case 'SET_ERROR':
+      return {
+        loading: false, 
+        error: "There are some errors",
+        todos: [],      
+      }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then(res => {
+        console.log(res.data);
+        dispatch({type:'SET_DATA', payload:res.data});
+      })
+      .catch(res =>{
+        dispatch({type:'SET_ERROR'})
+      })
+  }, [])
+  const listmarkup = (
+    <ul>
+      {state.todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
+  )
+  return (
+    <div className="App">
+      { state.loading?'ĺoading': (state.error?state.error:listmarkup) }
+    </div>
+  );
+}
+
+export default App;
